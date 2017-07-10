03c81b52ab464ed0884eb948ac3797825ee86fd9
trucsphuongw <osxvn@trucphuong.local>
2017-07-10 21:32:16 +0700

    Initial Commit

---
+++ Dynamic - Tính động trong Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dynamic - Tính động trong Objective-C
+//
+//  Created by Trúc Phương >_< on 10/07/2017.
+//  Copyright © 2017 iDev Bao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dynamic - Tính động trong Objective-C/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Dynamic - Tính động trong Objective-C
+//
+//  Created by Trúc Phương >_< on 10/07/2017.
+//  Copyright © 2017 iDev Bao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
